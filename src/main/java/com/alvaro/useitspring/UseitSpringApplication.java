b0463caa62ec3d3c6b1a25e5d7776ccbfbc4de47
Alvaro Ortega <alortegama@outlook.com>
2019-05-19 20:41:01 +0200

    Add new Agenda at init of server

--- src/main/java/com/alvaro/useitspring/UseitSpringApplication.java
+++ src/main/java/com/alvaro/useitspring/UseitSpringApplication.java
@@ -1,11 +1,39 @@
 package com.alvaro.useitspring;
 
+import com.alvaro.useitspring.models.dao.IAgendaDao;
+import com.alvaro.useitspring.models.entity.Agenda;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 @SpringBootApplication
-public class UseitSpringApplication {
+public class UseitSpringApplication implements CommandLineRunner {
+	@Autowired
+	IAgendaDao agendaDao;
+
 	public static void main(String[] args) {
 		SpringApplication.run(UseitSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Agenda agenda = new Agenda();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+		String dateInString = "25-11-1993 10:20:56";
+		try {
+			agenda.setDate(sdf.parse(dateInString));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		agenda.setDescription("Cumple Alvaro");
+		agenda.setLocation("Andújar (Jaén)");
+
+		agendaDao.save(agenda);
+
 	}
 }
